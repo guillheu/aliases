ae8f485423c344ef014309c7fde3e30a63154cb0
Guillaume Heu <72808144+guillheu@users.noreply.github.com>
2023-02-24 16:32:13 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -20,7 +20,7 @@ while true; do
     block=$(curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $1 | jq '.result.number'  | tr -d '"' | echo "$((16#$(cut -c 3-)))")
     peer=$(curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":2}' $1 | jq '.result'  | tr -d '"' | echo "Peer count : $((16#$(cut -c 3-)))")
     block_formatted=$(printf "Block number: %'d" $block)
-    printf "$block_formatted | $peer \r"
+    echo -ne "$block_formatted | $peer \r"
     sleep 1
 done
 };
