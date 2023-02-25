9c2fce6c390f3b6c369f12d0f4b4611f6ef9c360
Guillaume Heu <72808144+guillheu@users.noreply.github.com>
2023-02-25 18:45:37 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -15,6 +15,7 @@ alias list-aliases='alias | awk -F"=" '\''$1 ~ /^alias / {print $1}'\'''
 alias dc-reset='function _docker-compose-reset-function() { if [[ $* == *"-v"* ]]; then docker-compose down -v && docker-compose up -d; else docker-compose down && docker-compose up -d; fi }; _docker-compose-reset-function()'
 alias docker-build-run='function _docker_build_run() { image_name=$1; shift; docker build -t $image_name . && docker run --rm "$@" $image_name; }; _docker_build_run'
 alias get-eth-info='_get_eth_info'
+alias start-http='python -m http.server $1'
 function _get_eth_info() {
 while true; do
     block=$(curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $1 | jq '.result.number'  | tr -d '"' | echo "$((16#$(cut -c 3-)))")
